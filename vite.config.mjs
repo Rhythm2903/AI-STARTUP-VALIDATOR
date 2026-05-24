de6399dad8e49aca90fac830967ef940edf8794f
Rhythm2903 <agarwalrhythm287@gmail.com>
2026-05-24 22:42:12 +0530

    fix: rename to vite.config.mjs to force strict ESM packaging

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,7 @@
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default {
+export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
@@ -10,4 +11,4 @@ export default {
       },
     },
   },
-};
+});
