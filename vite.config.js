43b76b26af8dc9826b9c6d00668e1398d88aa1ae
Rhythm2903 <agarwalrhythm287@gmail.com>
2026-05-24 22:33:44 +0530

    fix: naked object export for vite config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [react()],
   server: {
     proxy: {
-      // This routes local frontend API calls to your local Python backend smoothly
       '/api': {
         target: 'http://127.0.0.1:8000',
         changeOrigin: true,
       },
     },
   },
-});
+};
